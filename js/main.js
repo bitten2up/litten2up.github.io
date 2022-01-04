96bb48253cff7fce01d9b145d038e293e094939a
litten 1up <seant9140@gmail.com>
2022-01-04 11:57:52 -0600

    commiting

--- js/main.js
+++ js/main.js
@@ -1,13 +1,7 @@
- var source = "sound/battle.ogg"
- var audio = document.createElement("audio");
- //
-
-function playaudio(){ 
- audio.autoplay = true;
- //
- audio.load()
- audio.addEventListener("load", function() { 
-     audio.play(); 
- }, true);
- audio.src = source;
+function html(text){
+    const e = document.createElement("div");
+    e.innerHTML = `<div id="scroll-container"><div id="scroll-text">${text}</div></div>`;
+    document.body.appendChild(e);
+    var scrollInterval = setInterval(function() { document.documentElement.scrollTop = document.documentElement.scrollHeight; }, 50);
+    
 }
